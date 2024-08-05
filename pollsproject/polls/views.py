dd42990aa1b4509303f8812611c2ba72d124b5b4
Yuvraj198920 <yuvvraaj0flying@gmail.com>
2024-08-05 23:31:50 +0530

    implemented 404 error

--- pollsproject/polls/views.py
+++ pollsproject/polls/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from .models import Question
 
-from django.template import loader
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
-
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You are looking at question {question_id}")
+    try:
+        question_detail = Question.objects.get(pk=question_id)
+        context = {"question": question_detail}
+    except Question.DoesNotExist:
+        raise Http404("Question does not exists!!")
+    return render(request, "polls/details.html", context)
 
 
 def results(request, question_id):
